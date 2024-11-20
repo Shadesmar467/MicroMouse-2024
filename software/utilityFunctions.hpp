c971bc6674e33af48622accd7a31df8f470a09a8
adamhwu <wuadam42@gmail.com>
2024-11-20 09:53:03 -0800

    commenting and cleaning up redundant code

--- software/utilityFunctions.hpp
+++ software/utilityFunctions.hpp
@@ -17,23 +17,12 @@ void initQ(Queue* q) {
 }
 
 CellList* getNeighborCells(Maze* mazePtr, Coord c) { //input a coordinate C, get back neighbors
-    
-    CellList* list = (CellList*)malloc(sizeof(CellList));
-    //this is a cell list named list allocating memory for all cells in the list
 
-    int dirX[] = {0, 1, 0, -1};
+    int dirX[] = {0, 1, 0, -1};     // used to find pos of neighbor cells
     int dirY[] = {1, 0, -1, 0};
 
-    int count = 4;  
-    char test[20];
-
-    //the following if statement is for calculating size of the allocated memory
-    if (c.x == 15 || 0)
-        count--;
-    if (c.y == 15 || 0)
-        count--;
-
-    list->cells = (Cell*)malloc(count * sizeof(Cell));
+    CellList* list = (CellList*)malloc(sizeof(CellList));     //this is a cell list named list allocating memory for all cells in the list
+    list->cells = (Cell*)malloc(list->size * sizeof(Cell));
     list->size = 0;
 
     //now i need to calculate the new coordinates of each cell and store the cell in the array
@@ -41,31 +30,29 @@ CellList* getNeighborCells(Maze* mazePtr, Coord c) { //input a coordinate C, get
         int newX = c.x + dirX[i];
         int newY = c.y + dirY[i];
 
-        bool currWall = (mazePtr->cellWalls[c.x][c.y] & dir_mask[i]);     // whether current cell has a wall blocking
-        bool neighborWall = (mazePtr->cellWalls[newX][newY] & ndir_mask[i]);       // whether neighbor cell has a wall blocking
+        // dir_mask checks order of 'nesw', ndir_mask checks order of 'swne' to match orientation of adjacent cell
+        bool currWall = (mazePtr->cellWalls[c.x][c.y] & dir_mask[i]);     // true if current cell has a wall between current and neighbor cell
+        bool neighborWall = (mazePtr->cellWalls[newX][newY] & ndir_mask[i]);       // true if neighbor cell has a wall current and neighbor cell
 
-        //checking if the coordinate is blocked or not
-        if (newX >= 0 && newX < 16 && newY >= 0 && newY < 16) { //if cell is in the maze boundaries (need to test if this works)
-            if (!currWall && !neighborWall){  // if new cell is blocked by a wall
+        if (newX >= 0 && newX < 16 && newY >= 0 && newY < 16) { //if cell is in the maze boundaries
+            if (!currWall && !neighborWall){  // if new cell is not blocked by a wall
                 list->cells[list->size].pos.x = newX;
                 list->cells[list->size].pos.y = newY; //then finally add to the cell list
                 list->size++; //increment cell list size
-
-                //std::cerr << "valid cell: (" << newX << "," << newY << ")" << std::endl;
             }
         }
     }
+
     return list;
 }
 
 Coord getBestCell(Maze* mazePtr, Mouse* mousePtr) {
-
-    Coord best_cell_coord;  // return object with coords of best cell
-    Coord current_coord = mousePtr->mousePos;   // current coords of mouse
-    CellList* neighbors = getNeighborCells(mazePtr, current_coord);     // calculate neighbors not blocked by walls
+    Coord best_cell_coord;  // return object
+    Coord current_coord = mousePtr->mousePos;
+    CellList* neighbors = getNeighborCells(mazePtr, current_coord);     // grab neighbors not blocked by walls
 
     int desired_cell_cost = mazePtr->distances[current_coord.x][current_coord.y] - 1;
-    std::cerr << "current: (" << current_coord.x << "," << current_coord.y << ")" << std::endl;
+    std::cerr << "current cell: (" << current_coord.x << "," << current_coord.y << ")" << std::endl;
 
     for (int i = 0; i < neighbors->size; i++) {     // iterate through the neighbors
         Coord test_cell_coord = neighbors->cells[i].pos;
@@ -73,11 +60,9 @@ Coord getBestCell(Maze* mazePtr, Mouse* mousePtr) {
 
         std::cerr << "valid cell: (" << test_cell_coord.x << "," << test_cell_coord.y << ")" << " cost:" << test_cell_cost << std::endl;
 
-        if (test_cell_cost == desired_cell_cost){   // if the cost is 1 less than the current one
+        if (test_cell_cost == desired_cell_cost)   // if the cost is 1 less than the current one
             best_cell_coord = test_cell_coord;
-        }
     }
-    std::cerr << "returning best cell from function" << std::endl << std:: endl;;
     return best_cell_coord;
 }
 
